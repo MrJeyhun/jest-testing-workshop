6b1c2d153631c2177cd54f17152357b870cc060a
Ceyhun S. Ceyhunzada <Ceyhun.Ceyhunzada@kapitalbank.az>
2022-11-10 18:00:40 +0400

    mocked getwinner function

--- other/whats-a-mock/__tests__/thumb-war.1.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.1.todo.js
@@ -1,103 +1,27 @@
 // monkey-patching
 import thumbWar from '../thumb-war'
-// import the utils module (see hint #1 at the bottom of the file)
+import * as utils from "../utils";
 
 test('returns winner', () => {
-  // keep track of the original `getWinner` utility function (see hint #2)
-  // overwrite the utils.getWinner function with
-  // our own that always returns the second player (see hint #3)
+  const orginalWinner = utils.getWinner
+  utils.getWinner = (...args) => {
+    utils.getWinner.mock.calls.push(args)
+    return args[1]
+  }
+  utils.getWinner.mock = {
+    calls: []
+  }
 
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
-  // change this assertion to be more for a specific player
-  // (like 'Kent C. Dodds', see hint #4):
-  expect(['Ken Wheeler', 'Kent C. Dodds'].includes(winner)).toBe(true)
+  expect(winner).toBe('Kent C. Dodds')
 
-  // restore the originalGetWinner function so other tests don't break
-  // (see hint #5)
+  // or .toHaveLength(2) as alternative
+  expect(utils.getWinner.mock.calls).toEqual(
+    [
+      [ 'Ken Wheeler', 'Kent C. Dodds' ],
+      [ 'Ken Wheeler', 'Kent C. Dodds' ]
+    ]
+  )
+  
+  utils.getWinner = orginalWinner
 })
-
-/*
-
-Hints below:
-
-
-
-
-
-
-See answers in the solution file
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Hint #1:
-
-import * as utils from '../utils'
-
-
-
-
-
-Hint #2:
-
-const originalGetWinner = utils.getWinner
-
-
-
-
-
-
-Hint #3:
-
-utils.getWinner = functionThatAlwaysReturnsPlayer2
-
-
-
-
-
-
-Hint #4:
-
-expect(winner).toBe('Kent C. Dodds')
-
-
-
-
-
-
-Hint #5:
-
-utils.getWinner = originalGetWinner
-
- */
